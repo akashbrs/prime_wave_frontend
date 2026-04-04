4fa9b5a66848b788bc9af4c47788e633743c0ccd
akashbrs <methukuakash092@gmail.com>
2026-04-04 14:30:40 +0530

    inserted spped insights of vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
       <Analytics />
+      <SpeedInsights />
     </ThemeProvider>
   </React.StrictMode>
 );
