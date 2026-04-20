9329d3c4762d016880269984565356fc03ab6ba1
akashbrs <methukuakash092@gmail.com>
2026-04-20 20:32:06 +0530

    updated 2

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -115,7 +115,7 @@ export default function Contact() {
 
       // 2. Send to Django Backend (FIXED)
       const response = await fetch(
-        "http://localhost:8005/api/contact/",
+        "https://api.prime-wave.tech/api/contact/",
         {
           method: "POST",
           headers: {
